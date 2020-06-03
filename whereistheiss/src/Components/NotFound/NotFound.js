51a6ebb52ec4f6ba0b52f5bd102c6f67968dbefc
Sam Crudge <Samuelcrudge@gmail.com>
2020-06-03 16:42:10 +0100

    fixed spacing on 404 page

--- whereistheiss/src/Components/NotFound/NotFound.js
+++ whereistheiss/src/Components/NotFound/NotFound.js
@@ -1,4 +1,4 @@
 import React from "react";
 import './NotFound.css'
-const NotFound = () => <h1>WOAH THERE You're at the end of Infinity!! 404!</h1>
+const NotFound = () => <h2>WOAH THERE You're at the end of Infinity!! 404!</h2>
 export default NotFound;
